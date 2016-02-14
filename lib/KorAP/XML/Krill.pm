dba47717400494a1c53b302ef1489f7141cdf5f4
Akron <nils@diewald-online.de>
2016-02-14 23:06:48 +0100

    Added archive support to korapxml2krill_dir
    Change-Id: Ib62934a08628db3667891a1562acbf0149c17482

--- lib/KorAP/XML/Krill.pm
+++ lib/KorAP/XML/Krill.pm
@@ -17,7 +17,7 @@ use File::Spec::Functions qw/catdir catfile catpath splitdir splitpath rel2abs/;
 #       Due to the kind of processing, processed metadata may be stored in
 #       a multiprocess cache instead.
 
-our $VERSION = '0.07';
+our $VERSION = '0.08';
 
 our @ATTR = qw/text_sigle
 	       doc_sigle
