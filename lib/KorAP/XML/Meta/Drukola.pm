b7f130c66058e90240bbe0771e40f7292e691947
Akron <nils@diewald-online.de>
2017-01-17 20:06:40 +0100

    Added DRuKoLa meta data skeleton
    Change-Id: Ief6eb81012766ee841eae0f745e64b0d9e98d3b4

---
+++ lib/KorAP/XML/Meta/Drukola.pm
@@ -0,0 +1,14 @@
+package KorAP::XML::Meta::Drukola;
+use KorAP::XML::Meta::Base;
+use Try::Tiny;
+
+# Parse meta data
+sub parse {
+  my $self = shift;
+  my $dom = shift;
+  my $type = shift;
+
+  return;
+};
+
+1;
